6f00b8aeb5188bcf648b909bece6b3c6c3886765
Jackson Qi <jacksonqi@jacksons-mbp.lan>
2024-02-23 12:08:21 -0800

    sync sorted merge solution. use a minHeap, and let the heap do the sorting.

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,35 @@
 "use strict";
 
-// Print all entries, across all of the sources, in chronological order.
+// Import the 'heap' module
+const Heap = require('heap');
 
 module.exports = (logSources, printer) => {
+
+  const minHeap = new Heap((a, b) => {
+    return a.log.date.getTime() - b.log.date.getTime();
+  });
+
+  // pop the first log from each source and push into heap
+  logSources.map(
+    (logSource, i) => minHeap.push({ log: logSource.pop(), logSourceIndex: i }))
+
+  // Until the heap is empty
+  while (!minHeap.empty()) {
+    // pop the top node from the heap, and print
+    const logNode = minHeap.pop()
+    let logEntry = logNode.log
+    let logSourceIndex = logNode.logSourceIndex
+    printer.print(logEntry)
+
+    // pop the source to refresh the status of .drained
+    let logSource = logSources[logSourceIndex];
+    let newEntry = logSources[logSourceIndex].pop()
+    // If not drained, newEntry is garanteed to be a log. Push to heap
+    if (!logSource.drained) {
+        minHeap.push({ log: newEntry, logSourceIndex: logSourceIndex });
+    }
+  }
+
+  printer.done()
   return console.log("Sync sort complete.");
 };
